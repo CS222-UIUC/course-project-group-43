5ab11b3cbcb5b59c997945130cd57404768ca263
jialinh4 <jialinh4@illinois.edu>
2022-10-07 20:31:08 -0500

    created timeline

---
+++ src/utils/timeline.js
@@ -0,0 +1,48 @@
+import browser from "webextension-polyfill";
+
+export default class Timeline {
+  constructor(){
+    // TODO
+  }
+
+  async _getLocalTime() {
+    // TODO
+  }
+
+  async _setLocalTime(time) {
+    // TODO
+  }
+
+  async _getSyncTime() {
+    // TODO
+  }
+
+  async _setSyncTime(time) {
+    // TODO
+  }
+
+  async _resetLocalTime(time) {
+    // TODO
+  }
+
+  async _resetSyncTime(time) {
+    // TODO
+  }
+
+  async getTimeline() {
+    // TODO
+  }
+
+  async setTimeline(timeline) {
+    // TODO
+  }
+
+  // helper funcs
+  async isEqual(timeline_1, timeline_2) {
+    // TODO
+  }
+
+  async mergeTimeline(timeline_1, timeline_2) {
+    // TODO
+  }
+}
