2e4942f2a85a047f2f34cc53f89b9d3adae6a325
changKq <73107259+changKq@users.noreply.github.com>
2022-10-28 21:48:49 -0500

    update countdown function and button link

--- src/background.js
+++ src/background.js
@@ -1,41 +1,52 @@
-// let color = '#3aa757';
+var btnStart=document.getElementById("btnStart");
+var btnPause=document.getElementById("btnPause");
+var btnBreak=document.getElementById("btnBreak");
+var btnCancel=document.getElementById("btnCancel");
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-//   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
-const timeH = document.querySelector("timer");
-const timeB = document.querySelector("break");
-let seconds;
+ var min=document.querySelectorAll("input")[0];
+ var is_cal=false;
+ // console.log(min);
+ var sec=document.querySelectorAll("input")[1];
+ var timer;
+ btnStart.onclick=Count;
+ btnPause.onclick=Pause;
+ btnCancel.onclick=Cancel;
+ btnBreak.onclick = Break;
 
-countDown(10,"timeH");
+ var audio= new Audio("https://wvw.mixloaded.com/wp-content/uploads/2022/09/Rosa_Walton_ft_Hallie_Coggins_-_I_Really_Want_to_Stay_at_Your_House_MixLoaded.Com.mp3");
+ function Count(){
+  is_cal=true;
+ }
+ function Pause(){
+  is_cal=false;
+ }
+ function Break(){
+  is_cal=false;
+  min.value=10;
+  sec.value=0;
+  audio.pause();
+ }
+  function Cancel(){
+  is_cal=false;
+  min.value=25;
+  sec.value=0;
+  audio.pause();
+ }
+ timer=setInterval(function(){
 
-function countDown({sec, link}){
-  seconds = sec;
-  seconds--;
-  displayTime(seconds,link);
-  if (seconds == 0 || seconds < 1) {
-      endCount(link);
+ if(is_cal){
+  if(sec.value>=1 && min.value>=0){
+    sec.value--
+  }else if(min.value>0){
+    sec.value=59;
+    min.value--
+  }else{
+    is_cal=false;
+    sec.value=min.value=0;
+    audio.play();
+    alert("Time out");
   }
-}
-
-
-function displayTime({second, link}) {
-  const min = Math.floor(second / 60);
-  const sec = Math.floor(second % 60);
-  if(link == "timeH"){
-     timeH.innerHTML = `
-  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
-  `;
-  }else if(link == "timeB"){
-    timeB.innerHTML = `
-  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
-  `;
-  }
-}
-
-
-  function endCount(link) {
-    link.innerHTML = "Time out";
-    alert("time out");
-  }
+ }else{
+ min.value=min.value;
+ }
+ },1000);
